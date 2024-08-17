ca23cd6786f5505d3edb4ef1602cb79414f47987
ashik <ashik.os@cied.in>
2024-08-17 15:51:19 +0530

    app .js updated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import { Main } from './pages';
+import { Routes, BrowserRouter as Router, Route, } from 'react-router-dom'
+import Layout from './components/shared/Layout'
+import Dashboard from './components/Dashboard'
+import Service from './components/Service'
+import Orders from './components/Orders'
+import Bills from './components/Bills'
+import Login from './pages/login/Login'
+import Customer from './components/Customer'
 
 function App() {
   return (
-    <div >
-          <Main />
+    <div className="flex flex-row h-screen w-full bg-slate-200">
+            <Router>
+              <Routes>
+                <Route path='/' element={<Layout/>}>
+                    <Route index element={<Dashboard/>} />
+                    <Route path='service' element={<Service/>} />
+                    <Route path='dashboard' element={<Dashboard/>} />
+                    <Route path='orders' element={<Orders/>} />
+                    <Route path='bills' element={<Bills/>} />
+                    <Route path='customers' element={<Customer/>} />
+                </Route>
+                <Route path='/login' element={<Login/>}>
+                  
+                  
+                </Route>
+              </Routes>
+            </Router>
+            
+            
     </div>
   );
 }
